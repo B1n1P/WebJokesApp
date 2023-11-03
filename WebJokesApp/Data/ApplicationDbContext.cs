50b99f9ad87b195864710f09aff5c23037da7440
51523 <51523@E306PC12>
2023-11-03 16:59:12 +0000

    Adicionar arquivos de projeto.

---
+++ WebJokesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebJokesApp.Models;
+
+namespace WebJokesApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebJokesApp.Models.Jokes> Jokes { get; set; } = default!;
+    }
+}
---
+++ WebJokesApp/Data/Migrations/20231103162030_AtualizarBD.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebJokesApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AtualizarBD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdUser",
+                table: "Jokes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdUser",
+                table: "Jokes");
+        }
+    }
+}
